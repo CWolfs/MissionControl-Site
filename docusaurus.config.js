bbd5ffbd598a9ca5ef3253e4c5ef1db1b292779f
Richard Griffiths <cwolfs@gmail.com>
2021-12-22 18:57:05 +0100

    Added v1.3.2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ module.exports = {
         { to: "docs/overview/about", label: "Docs", position: "left" },
         { to: "blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/CWolfs/MissionControl/releases/download/v1.3.1/MissionControl-v1.3.1.zip",
-          label: "Download (1.3.1)",
+          href: "https://github.com/CWolfs/MissionControl/releases/download/v1.3.2/MissionControl-v1.3.2.zip",
+          label: "Download (1.3.2)",
           position: "left",
         },
         {
