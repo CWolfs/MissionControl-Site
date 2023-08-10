5f07234d77908b7c53b68cee62799c12d0dc11a6
Richard Griffiths <cwolfs@gmail.com>
2023-08-10 16:33:44 +0200

    v1.5.2 release blog post

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ module.exports = {
         { to: "docs/overview/about", label: "Docs", position: "left" },
         { to: "blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/CWolfs/MissionControl/releases/download/v1.5.1/MissionControl-v1.5.1.zip",
-          label: "Download (1.5.1)",
+          href: "https://github.com/CWolfs/MissionControl/releases/download/v1.5.2/MissionControl-v1.5.2.zip",
+          label: "Download (1.5.2)",
           position: "left",
         },
         {
